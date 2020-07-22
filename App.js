e0c607336bfb2326b8196092781510d44deb2784
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-22 19:26:58 +0200

    Updating depndencies and changing code make it work again

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation';
-import HomeScreen from './screens/HomeScreen';
-import ModalsScreen from './screens/ModalsScreen';
-import GesturesScreen from './screens/GesturesScreen';
-import FormsScreen from './screens/FormsScreen';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import ModalsScreen from "./screens/ModalsScreen";
+import GesturesScreen from "./screens/GesturesScreen";
+import FormsScreen from "./screens/FormsScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
-const RootStack = createStackNavigator(
-  {
-    Home: { screen: HomeScreen },
-    Modals: { screen: ModalsScreen },
-    Forms: { screen: FormsScreen },
-    Gestures: { screen: GesturesScreen }
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
+const Stack = createStackNavigator();
 
-class App extends React.Component {
-  render() {
-    return <RootStack />
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Forms" component={FormsScreen} />
+        <Stack.Screen name="Modals" component={ModalsScreen} />
+        <Stack.Screen name="Gestures" component={GesturesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
